2f85ddb519a68606c6030d29758198d3f123397a
khangtictoc <tranhoangkhang09112001@gmail.com>
2025-01-29 21:22:32 +0700

    Adjust portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { FaBars, FaHome, FaUser, FaProjectDiagram, FaTools, FaTrophy, FaGraduationCap } from 'react-icons/fa';
 import './App.css';
+import profileImg from './assets/profile.png';
 
 function App() {
   const [showNav, setShowNav] = useState(false);
@@ -15,20 +16,21 @@ function App() {
         <FaBars size={30} />
       </div>
       <div className={`nav-menu ${showNav ? 'show' : ''}`}>
-        <a href="#home"><FaHome /> Home</a>
-        <a href="#about"><FaUser /> About Me</a>
-        <a href="#project"><FaProjectDiagram /> Project</a>
-        <a href="#techstack"><FaTools /> Techstack</a>
-        <a href="#achievement"><FaTrophy /> Achievement</a>
-        <a href="#education"><FaGraduationCap /> Education</a>
+        <a href="#home"><FaHome />Home</a>
+        <a href="#about"><FaUser />About Me</a>
+        <a href="#project"><FaProjectDiagram />Project</a>
+        <a href="#techstack"><FaTools />Techstack</a>
+        <a href="#achievement"><FaTrophy />Achievement</a>
+        <a href="#education"><FaGraduationCap />Education</a>
       </div>
       <div className="main-content">
-        <img src="/profile.jpg" alt="Profile" className="profile-image" />
-        <h1>Hi! I am <u>KHANG HOANG TRAN</u></h1>
+        <img src={profileImg} alt="Profile" className="profile-image" />
+        <h1>Welcome! I'm <u>KHANG HOANG TRAN</u></h1>
         <h2>DevSecOps Expert</h2>
       </div>
     </div>
   );
 }
+
 
 export default App;
